abb32bb43f16512a09f65b52ea5d44775419c7ba
stinamysen <stinamysen@live.no>
2019-10-22 18:46:11 +0200

    h

--- P3/skjemavalidering.js
+++ P3/skjemavalidering.js
@@ -1,12 +1,29 @@
-function validateForm() {
-    var x = document.forms["myform"].value;
-    if (x == "") {
-      alert("Alle felt må fylles inn");
-      return false;
-    }
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("trigger");
+  // Get the output text
+  var identitet = document.getElementById("navn");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    identitet.style.display = "block";
+  } else {
+    identitet.style.display = "none";
   }
+}
+function validateForm() {
+  var name = document.forms["rykteskjema"]["navn"];
+  var rykte = document.forms["rykteskjema"]["rykte"];
 
-function showInputField(){
-  document.getElementById("skjult").style.display = ""
+  if (name.value ==""){
+    window.alert("Vennsligst skriv inn ditt navn.");
+    name.focus();
+    return false;  
+  }
+   if (rykte.value ==""){
+    window.alert("Vennsligst del ryktet.");
+    rykte.focus();
+    return false;
+}  
+}
   
-
