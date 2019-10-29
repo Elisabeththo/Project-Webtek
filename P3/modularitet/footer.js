05da0b9aa0bc0927f849526bf51fa1b0f1d2718f
Didrik Singsaas Magnussen <didrik@singsaas-magnussen.no>
2019-10-29 13:41:25 +0100

    endr

--- P3/modularitet/footer.js
+++ P3/modularitet/footer.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 document.getElementById("liste").innerHTML= 'adresse';
 document.getElementById("liste").innerHTML= 'mail';
 document.getElementById("liste").innerHTML= 'tlf';
+=======
+document.getElementById("tohnnasfooter").innerHTML= 'Adresse: Klæbuveien 72,7030 Trondheim <pre> Email: tohnna.stoh@gmail.com <pre> Tlf: 920 28 440';
+>>>>>>> faa33b0eced23aebd0122fbbed95cfb4cd41230f
