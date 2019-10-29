b5b02a175e6a2649045d18400819e7f0c04d810f
stinamysen <stinamysen@live.no>
2019-10-29 10:47:16 +0100

    h

--- P3/modularitet/footer.js
+++ P3/modularitet/footer.js
@@ -1,3 +1,3 @@
-document.getElementById("tohnnasfooter").innerHTML = "Adresse: Klæbuveien 72, 7030 Trondheim"
-document.getElementById("tohnnasfooter").innerHTML = "Email: tohnna.stoh@gmail.com"
-document.getElementById("tohnnasfooter").innerHTML = "Tlf: 920 28 440"
+document.getElementsByClassName("tohnnasfooter").innerHTML = "Adresse: Klæbuveien 72, 7030 Trondheim"
+document.getElementsByClassName("tohnnasfooter").innerHTML = "Email: tohnna.stoh@gmail.com"
+document.getElementsByClassName("tohnnasfooter").innerHTML = "Tlf: 920 28 440"
