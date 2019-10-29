cfaaf81de766a232042d0fad5d58dc3d7c7ec31f
stinamysen <stinamysen@live.no>
2019-10-29 13:59:57 +0100

    h

--- P3/modularitet/footer.js
+++ P3/modularitet/footer.js
@@ -1,1 +1,1 @@
-document.getElementById("tohnnasfooter").innerHTML= 'Adresse: Klæbuveien 72,7030 Trondheim <pre> Email: tohnna.stoh@gmail.com <pre> Tlf: 920 28 440';
+document.getElementById("tohnnasfooter").innerHTML= '<br>Adresse: Klæbuveien 72,7030 Trondheim <br> Email: tohnna.stoh@gmail.com <br> Tlf: 920 28 440';
