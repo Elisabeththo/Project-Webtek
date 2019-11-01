cb2090818fe2348ed96493e155b1860c9f0cdff4
stinamysen <stinamysen@live.no>
2019-11-01 16:24:45 +0100

    hei

--- P3/modularitet/footer.js
+++ P3/modularitet/footer.js
@@ -1,1 +1,12 @@
-document.getElementById("tohnnasfooter").innerHTML= '<br>Adresse: Klæbuveien 72,7030 Trondheim <br> Email: tohnna.stoh@gmail.com <br> Tlf: 920 28 440';
+let footer = document.getElementById("tohnnasfooter")
+footer.innerHTML = 'Adresse: Klæbuveien 72,7030 Trondheim <br> Email: tohnna.stoh@gmail.com <br> Tlf: 920 28 440<br>';
+
+//Lager et bildeelement til hvert av ikonene
+var instagramIcon = document.createElement('img');
+var facebookIcon = document.createElement('img');
+
+//Lager et linkelement for linkene
+var instagramLink = document.createElement('a');
+var facebookLink = document.createElement('a');
+
+//Ikonene
