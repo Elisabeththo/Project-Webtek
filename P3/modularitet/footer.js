5976a7b6d3478d43edd1b9bbd12ef6bf5cc9e069
stinamysen <stinamysen@live.no>
2019-11-01 17:14:05 +0100

    h

--- P3/modularitet/footer.js
+++ P3/modularitet/footer.js
@@ -2,27 +2,20 @@ let footer = document.getElementById("tohnnasfooter")
 footer.innerHTML = 'Adresse: Klæbuveien 72,7030 Trondheim <br> Email: tohnna.stoh@gmail.com <br> Tlf: 920 28 440<br>';
 
 //Lager et bildeelement til hvert av ikonene
-var instagramIcon = document.createElement('img');
 var facebookIcon = document.createElement('img');
 
-//Lager et linkelement for linkene
-var instagramLink = document.createElement('a');
+//Lager et linkelement for linken
 var facebookLink = document.createElement('a');
 
-//Ikonene
-instagramIcon.src = "./img/instagramIcon.png";
+//Ikonen
 facebookIcon.src = "./img/facebookIcon.png";
 
-//Legger til link, åpner ny fane
-instagramLink.href = "http://instagram.com/tohnnastoh/";
-instagramLink.target = "_blank";
+//Legger til linken, åpner ny fane
 facebookLink.href = "http://facebook.com/tohnna/";
 facebookLink.target = "_blank";
 
-//Putter ikonene til linkene
-instagramLink.appendChild(instagramIcon);
+//Putter ikonene til linken
 facebookLink.appendChild(facebookIcon);
 
-//Legger til både ikonene og linkene til selve footeren
-footer.appendChild(instagramLink);
+//Legger til både ikonen og linken til selve footeren
 footer.appendChild(facebookLink);
